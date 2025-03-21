60231f5c11b73cf19d8c69fb91309bfca0d08d4d
kuppr <prashant.kukreti@cognizant.com>
2025-03-21 15:09:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,81 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.x"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
 provider "aws" {
-  region  = "us-west-2"
-}
-resource "aws_instance" "example_server" {
-  ami           = "ami-00c257e12d6828491"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "example_server"
-  }
+  region = "us-east-1"
 }
 
+resource "aws_elastic_beanstalk_application" "myelasticapp" {
+  name        = var.myelasticapp
+  description = "My Test App"
+}
+
+resource "aws_elastic_beanstalk_environment" "beanstalkappenv" {
+  name                = var.beanstalkappenv
+  application         = aws_elastic_beanstalk_application.myelasticapp.name
+  solution_stack_name = var.solution_stack_name
+  tier                = var.tier
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCID"
+    value     = var.vpc_id
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     = "True"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", var.public_subnets)
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+  }
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 2
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+}
