8e7042f5323c6b55717c22a2b5983fccd6ef3d09
kuppr <prashant.kukreti@cognizant.com>
2025-03-21 14:47:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.x"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region  = "us-west-2"
+}
+resource "aws_instance" "example_server" {
+  ami           = "ami-00c257e12d6828491"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "example_server"
+  }
+}
+
